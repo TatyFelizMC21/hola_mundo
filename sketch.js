6c90b668c50bdf84c67417be25639e707f21720e
Tatiana Moreno <telizabeth.morenoc@ucuenca.edu.ec>
2024-09-26 16:03:54 -0500

    Se creó un nuevo cursor con círculo y rectángulo con el profe

--- sketch.js
+++ sketch.js
@@ -1,14 +1,21 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  frameRate(2);
+  frameRate(50);
   background(0);
   rectMode(CENTER);
 }
 
 function draw() {
-  background(random(50, 100), random(255, 0), random(150, 200));
+  background(20, random(0, 100), 255, 10);
+
+  strokeWeight(5);
+  noFill();
+  stroke(mouseY, 255, 0);
   fill(0, 20, 200);
   circle(mouseX + 80, mouseY, mouseX);
+
+  strokeWeight(5);
+  stroke(0, 255, 0);
   fill(100, 200, 20);
   rect(mouseX, mouseY, 100, 100);
 }

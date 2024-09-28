26cb1fdce43d3110827cf87f8d1a24e0d3533905
Tatiana Moreno <telizabeth.morenoc@ucuenca.edu.ec>
2024-09-27 22:45:12 -0500

    Se agregó un corazón
    Este tiene un efecto de pulsación :3

--- sketch.js
+++ sketch.js
@@ -20,18 +20,28 @@
 //rect(mouseX, mouseY, 100, 100);
 //}
 
+let heart; // Declaración de heart globalmente
+
+function preload() {
+  // Carga la imagen del corazón en formato base64
+  heart = loadImage(
+    "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAcAAAAHCAYAAADEUlfTAAAAAXNSR0IArs4c6QAAAEZJREFUGFd9jcsNACAIQ9tB2MeR3YdBMBBq8CIXPi2vBICIiOwkOedatllqWO6Y8yOWoyuNf1GZwgmf+RRG2YXr+xVFmA8HZ9Mx/KGPMtcAAAAASUVORK5CYII="
+  );
+}
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   frameRate(50);
-  background(20);
+  background(20); // Fondo negro
   describe(
     "A yellow circle on a black background. The circle opens and closes its mouth."
   );
 }
 
 function draw() {
-  background(0);
+  background(20, random(0, 100));
 
+  //CÓDIGO DEL PACMAN*****************************************************
   noStroke();
   fill(255, 255, 0); // Color amarillo para Pacman
 
@@ -39,11 +49,19 @@ function draw() {
   let startAngle = biteSize * sin(frameCount * 0.1) + biteSize;
   let endAngle = TWO_PI - startAngle;
 
-  arc(mouseX, mouseY, 80, 80, startAngle, endAngle, PIE); // Dibuja el arco de Pacman
+  // Arco de Pacman donde está el mouse
+  arc(mouseX, mouseY, 80, 80, startAngle, endAngle, PIE);
 
-  strokeWeight(5);
-  noFill();
-  stroke(mouseY, 255, 0);
-  fill(0, 20, 200);
-  circle(mouseX + 80, mouseY, 30);
+  //CÓDIGO DEL CORAZÓN*****************************************************
+  noSmooth(); //Desactiva el suavizado
+
+  // Efecto de palpitación para el corazón
+  let pulse = sin(frameCount * 0.1) * 10 + 40;
+
+  // Posición del corazón a la derecha de Pacman
+  let heartX = mouseX + 80;
+  let heartY = mouseY - 15;
+
+  // Efecto de palpitación
+  image(heart, heartX, heartY, pulse, pulse);
 }
